36c68ffbf89e79728a5e0bb4b96523dae0c26d97
MingchenInSZ <mingchen0710@126.com>
2015-04-12 21:08:05 +0800

    Add csv file operation

--- semsimilarity/src/sim/util/pathwaySim.java
+++ semsimilarity/src/sim/util/pathwaySim.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
@@ -13,6 +14,8 @@ import java.util.Map;
 import java.util.Vector;
 
 import sim.wang.SemSimDump;
+
+import com.csvreader.CsvWriter;
 
 /**
  * 
@@ -70,7 +73,7 @@ public class pathwaySim {
 			HashMap<String, LinkedHashSet<String>> map, int min, int max) {
 		Iterator<String> it = map.keySet().iterator();
 		Vector<String> vector = new Vector<String>();
-		while(it.hasNext()){
+		while (it.hasNext()) {
 			String path = it.next();
 			if (map.get(path).size() < min | map.get(path).size() > max) {
 				vector.add(path);
@@ -83,10 +86,45 @@ public class pathwaySim {
 		return map;
 	}
 
-	public void pathwaySemSim(String fileName) throws ClassNotFoundException,
+	/**
+	 * 
+	 * @param fileName
+	 * @return HashMap
+	 * @throws IOException
+	 * 
+	 *             This class prepare the significant pathway dataset for
+	 *             similarity calculation
+	 */
+	public HashMap<String, LinkedHashSet<String>> getSigPathway(String fileName)
+			throws IOException {
+		HashMap<String, LinkedHashSet<String>> total = getPathways("cpdbpathway.gmt");
+		HashMap<String, LinkedHashSet<String>> map = new HashMap<String, LinkedHashSet<String>>();
+		BufferedReader br = new BufferedReader(new FileReader(new File(wdir
+				+ fileName)));
+		String line = br.readLine();
+		while (line != null) {
+			map.put(line, total.get(line));//from the pathway dataset get the sigpathway data
+			line = br.readLine();
+		}
+		br.close();
+
+		return map;
+	}
+
+	/**
+	 * 
+	 * @param fileName
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 * @throws InterruptedException
+	 * 
+	 *             This method calculate the pathway similarity in multi thread
+	 *             model
+	 */
+	public void pathwaySemSimMulti(String fileName) throws ClassNotFoundException,
 	IOException, InterruptedException {
-		HashMap<String, LinkedHashSet<String>> map = pathwayFilter(
-				getPathways(fileName), 5, 200);
+		HashMap<String, LinkedHashSet<String>> map = getSigPathway(fileName);
+		// pathwayFilter(getPathways(fileName), 5, 200);
 		// Map<String, HashMap<String, Float>> dump = new HashMap<String,
 		// HashMap<String, Float>>();
 		bw = new BufferedWriter(new FileWriter(
@@ -94,19 +132,113 @@ public class pathwaySim {
 		SemSimDump sem = new SemSimDump();
 		Object[] paths = map.keySet().toArray();
 		System.out.println("Starting ...");
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < paths.length; i++) {
 			new innerThread(map, paths, sem, i).start();
 			// Thread.sleep(100000);
 		}
+		// bw.close();
 
 	}
 
+	/**
+	 * 
+	 * @param fileName
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 * @throws InterruptedException
+	 * 
+	 *             Calculate the pathway simiarity one by one without dump
+	 */
+	public void pathwaySemSim(String fileName) throws ClassNotFoundException,IOException,InterruptedException{
+		HashMap<String, LinkedHashSet<String>> map = getSigPathway(fileName);
+		//bw = new BufferedWriter(new FileWriter(new File(wdir + "pathwaysim.res")));
+		CsvWriter cw = new CsvWriter(wdir + "pathwaysim.csv", ',',
+				Charset.forName("UTF-8"));
+		SemSimDump sem = new SemSimDump();
+		Object[] paths = map.keySet().toArray();
+		System.out.println("Starting ...");
+		for (int i = 0; i < paths.length; i++) {
+			for (int j = i + 1; j < paths.length; j++) {
+				Map<String, HashMap<String, Float>> dump = new HashMap<String, HashMap<String, Float>>();
+				Object[] geneset1 = map.get(paths[i]).toArray();
+				Object[] geneset2 = map.get(paths[j]).toArray();
+				float gs1 = 0.0f, gs2 = 0.0f;
+				for (int ii = 0; ii < geneset1.length; ii++) {
+					float tmp = 0.0f;
+					for (int jj = 0; jj < geneset2.length; jj++) {
+						float sim = 0.0f;
+
+						try {// calculate sim
+							sim = sem.geneSemSim(geneset1[ii].toString(),
+									geneset2[jj].toString());
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+						if (tmp < sim) {
+							tmp = sim;
+						}
+						// dump for alternative summation
+						if (dump.containsKey(geneset2[jj].toString())) {
+							HashMap<String, Float> inner = dump
+									.remove(geneset2[jj].toString());
+							inner.put(geneset1[ii].toString(),
+									Float.valueOf(sim));
+							dump.put(geneset2[jj].toString(), inner);
+						} else {
+							HashMap<String, Float> inner = new HashMap<String, Float>();
+							inner.put(geneset1[ii].toString(),
+									Float.valueOf(sim));
+							dump.put(geneset2[jj].toString(), inner);
+						}
+					}
+					gs1 += tmp;
+				}// end for
+
+				// sum all the max sim in geneset2 to everyone in geneset1
+				int ii = 0;
+				gs2 = 0.0f;
+				for (Object gene : geneset2) {
+					float tmp = 0.0f;
+					for (Object g : geneset1) {
+						if (tmp < dump.get(gene.toString()).get(g.toString())
+								.floatValue()) {
+							tmp = dump.get(gene.toString()).get(g.toString())
+									.floatValue();
+						}
+					}
+					gs2 += tmp;
+				}
+				float psim = (gs1 + gs2)
+						/ (geneset1.length + geneset2.length + 0.0001f);
+				System.out.println(i + " " + j + " " + paths.length + " "
+						+ psim);
+				String [] contents = {paths[i].toString(),paths[j].toString(),Float.valueOf(psim).toString()};
+				try {
+					cw.writeRecord(contents);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}// end for
+		}
+		cw.close();
+	}
 	public static void main(String[] args) throws InterruptedException,
 	ClassNotFoundException, IOException {
 
 		pathwaySim psim = new pathwaySim();
-
-		psim.pathwaySemSim("cpdbpathway.gmt");
+		// HashMap<String, LinkedHashSet<String>> map = psim
+		// .getPathways("cpdbpathway.gmt");
+		psim.pathwaySemSim("sigpathway.txt");
+		// for(String s:map.keySet()){
+		// System.out.println(s+"----¡·"+map.get(s).size());
+		// }
+		// System.out.println(psim.getSigPathway("sigpathway.txt").size());
 	}
 
 	/**
@@ -114,6 +246,7 @@ public class pathwaySim {
 	 * @author mingchen
 	 * @date 2015.4.11
 	 * 
+	 *       Inner class for multi threading
 	 */
 	class innerThread extends Thread {
 		// inner thread local variables
@@ -216,4 +349,3 @@ public class pathwaySim {
 
 	}
 }
-
